b11ed615abe52525ed5975d457f05ad6a3c4b905
wuranbo <wuranbo@gmail.com>
2015-05-06 17:37:53 +0800

    *moveon) 增加unite定制，方便只关心一个git库的一部分代码。

--- scripts/vim_avro.sh
+++ scripts/vim_avro.sh
@@ -10,11 +10,12 @@ fi
 
 cd /Users/wrb/goworkspace/src/github.com/wuranbo/avro
 bash ~/github/wuranbo/vim/scripts/ctags_avro.sh
-vim --cmd "set path+=~/goworkspace/src/github.com/wuranbo/avro" \
+cd /Users/wrb/goworkspace/src/github.com/wuranbo/avro/lang/py
+vim --cmd "set path+=~/goworkspace/src/github.com/wuranbo/avro/lang/py" \
   --cmd "autocmd BufNewFile,BufRead *.tpl set filetype=html" \
   --cmd "autocmd BufNewFile,BufRead *.sample set filetype=dosini" \
-  -c "nnoremap <silent> <F5>  :<C-u>Unite -smartcase -buffer-name=files -start-insert file_rec/git:/Users/wrb/github/wuranbo/avro/:<CR>" \
-  -c "nnoremap <F3>  :<C-u>Unite -smartcase -start-insert grep:/Users/wrb/github/wuranbo/avro/:--python<CR>" \
+  -c "nnoremap <silent> <F5>  :<C-u>Unite -smartcase -buffer-name=files -start-insert file_rec/git:/Users/wrb/github/wuranbo/avro/lang/py:<CR>" \
+  -c "nnoremap <F3>  :<C-u>Unite -smartcase -start-insert grep:/Users/wrb/github/wuranbo/avro/lang/py:--python<CR>" \
   --cmd "autocmd VimLeavePre * :mksession!  ~/.vim_sessions/avro" \
   --cmd "autocmd VimLeavePre * :wviminfo ~/.vim_viminfos/avro" \
   --cmd "autocmd VimEnter * :rviminfo ~/.vim_viminfos/avro" \
